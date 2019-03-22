57e85521c68417ccfad0cd8f1ee5f58a4f50489b
Romain Torrente <romain.torrente@insa-lyon.fr>
2019-03-22 15:16:16 +0100

    change url

--- checker/urls.py
+++ checker/urls.py
@@ -8,5 +8,5 @@ from .models import CheckPlace
 urlpatterns = [
     path('', login_required(ListView.as_view(model=CheckPlace,)), name='list_check_place'),
     path('add_check/<int:pk>', login_required(views.add_check), name='add_check'),
-    path('screen/<str:token_screen>', views.screen, name='screen'),
+    path('display/<str:token_screen>', views.screen, name='screen'),
 ]
