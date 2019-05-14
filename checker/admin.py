592ff94a4b75702f9306cbaf4de8cdca884133cb
rtorrente <romain.torrente@gmail.com>
2019-05-14 11:48:45 +0200

    Modification page admin des check

--- checker/admin.py
+++ checker/admin.py
@@ -5,6 +5,13 @@ from .models import *
 
 
 # Register your models here.
-@admin.register(CheckPlace, Check, Screen)
+@admin.register(CheckPlace, Screen)
 class Admin(admin.ModelAdmin):
     pass
+
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = '__str__', 'seems_legit', 'check_place', 'created_at'
+    list_display_links = '__str__',
+    search_fields = 'student_id', 'created_at'
+    list_filter = 'check_place', 'seems_legit'
